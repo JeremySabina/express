0e1fe462271f5c90ba0de6d19c711246c03ca1a0
Jeremy Sabina <jeremysabina@gmail.com>
2024-09-29 09:21:24 -0400

    added express folder

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// setup static folder
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", 'index.html'));
+})
+
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", 'about.html'));
+})
+
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+
+app.listen(8000, () => console.log(`Server is running on port 8000`))
